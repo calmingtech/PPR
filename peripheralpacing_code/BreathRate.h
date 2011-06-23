77e526ee47fa5d9e07e5ccd3a4d4e2583fc8c1be
Poorna Krishnamoorthy <poorna@Poorna-Balakrishnans-MacBook-Pro.local>
2011-06-23 09:23:48 -0700

    fixed breathrate to be consistent with Yaser's method

--- peripheralpacing_code/BreathRate.h
+++ peripheralpacing_code/BreathRate.h
@@ -16,18 +16,22 @@
 {
 @private 
 	//a circular buffer for storing the last ~33 seconds of breath data (at a sampling rate of 18samples/sec)
-	int samples[NUM_SAMPLES];
+	float samples[NUM_SAMPLES];
+	//a circular buffer for storing time at which sample was taken
+	float sampleTime[NUM_SAMPLES];
+
 	int sample_index;
 	//a circular buffer for storing the last WINDOW_SIZE samples for smoothing
 	int smoothing_buffer[WINDOW_SIZE];
 	int smoothing_index;
 	//the last sample time, for computing a running average sample rate
 	float last_sample_time;
-	float average_sample_period;
+//	float average_sample_period;
 	//tells us whether or not there is enough data for the breath rate window
 	bool warm_up;
 	//last calculated breath rate (cycles/min)
 	float breath_rate;
+	
 	//hann window used for smoothing
 }
 
@@ -36,6 +40,8 @@
 - (float) hann: (int) idx; 
 
 - (float) getBreathRate;
+
+
 //adds the new sample to the data and calculates new average sample rate, and breath rate based on that
 - (id) add_sample: (int) value :(float) time;
 
